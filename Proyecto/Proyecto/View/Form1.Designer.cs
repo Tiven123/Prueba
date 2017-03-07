8f3769fd17f17863f02372f9af74a7d3e947eab5
steven alejandro ortiz alvarez <steven.alejandro@hotmail.com>
2017-03-06 22:08:02 -0600

    Vista

--- Proyecto/Proyecto/View/Form1.Designer.cs
+++ Proyecto/Proyecto/View/Form1.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(670, 416);
+            this.Name = "Form1";
+            this.Text = "Menu Principal";
+            this.ResumeLayout(false);
+
         }
 
         #endregion

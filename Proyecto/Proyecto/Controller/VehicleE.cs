249538e1b9ea5c0b8739ce625ea8b9703d7fd86c
steven alejandro ortiz alvarez <steven.alejandro@hotmail.com>
2017-03-16 00:21:55 -0600

    vehicles

---
+++ Proyecto/Proyecto/Controller/VehicleE.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public class VehicleE
+    {
+        string id;
+        int model;
+        string client;
+        int year;
+        string engine;
+        string chassis;
+        char fuel;
+        string type;
+        int capacity;
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public int Model
+        {
+            get
+            {
+                return model;
+            }
+
+            set
+            {
+                model = value;
+            }
+        }
+
+        public string Client
+        {
+            get
+            {
+                return client;
+            }
+
+            set
+            {
+                client = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+
+            set
+            {
+                year = value;
+            }
+        }
+
+        public string Engine
+        {
+            get
+            {
+                return engine;
+            }
+
+            set
+            {
+                engine = value;
+            }
+        }
+
+        public string Chassis
+        {
+            get
+            {
+                return chassis;
+            }
+
+            set
+            {
+                chassis = value;
+            }
+        }
+
+        public char Fuel
+        {
+            get
+            {
+                return fuel;
+            }
+
+            set
+            {
+                fuel = value;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+
+            set
+            {
+                type = value;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+
+            set
+            {
+                capacity = value;
+            }
+        }
+
+        public VehicleE(string id, int model, string client, int year, string engine, string chassis, char fuel, string type, int capacity)
+        {
+            this.Id = id;
+            this.Model = model;
+            this.Client = client;
+            this.Year = year;
+            this.Engine = engine;
+            this.Chassis = chassis;
+            this.Fuel = fuel;
+            this.Type = type;
+            this.Capacity = capacity;
+        }
+
+        public VehicleE()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                "Codigo: " + this.id + "\n" +
+                "Modelo: " + this.model + "\n" +
+                "Cliente: " + this.client + "\n" +
+                "Anno: " + this.year + "\n" +
+                "Motor: " + this.engine + "\n" +
+                "Chasis" + this.chassis + "\n" +
+                "Comblustible: " + this.fuel + "\n" +
+                "Tipo: " + this.type + "\n" +
+                "Capacidad: " + this.capacity;
+        }
+    }
+}
---
+++ Proyecto/Proyecto/View/Vehicles.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Proyecto.View
+{
+    partial class Vehicles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbModel = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbCliente = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEnngine = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtChassis = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ckE = new System.Windows.Forms.CheckBox();
+            this.ckD = new System.Windows.Forms.CheckBox();
+            this.ckG = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtCapacity = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(45, 471);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Eliminar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(45, 442);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Modificar";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(45, 413);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 9;
+            this.btnInsert.Text = "Insertar";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(172, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(642, 481);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Placa";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(16, 30);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(150, 20);
+            this.txtCode.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Modelo";
+            // 
+            // cbModel
+            // 
+            this.cbModel.FormattingEnabled = true;
+            this.cbModel.Location = new System.Drawing.Point(16, 73);
+            this.cbModel.Name = "cbModel";
+            this.cbModel.Size = new System.Drawing.Size(150, 21);
+            this.cbModel.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Cliente";
+            // 
+            // cbCliente
+            // 
+            this.cbCliente.FormattingEnabled = true;
+            this.cbCliente.Location = new System.Drawing.Point(16, 111);
+            this.cbCliente.Name = "cbCliente";
+            this.cbCliente.Size = new System.Drawing.Size(150, 21);
+            this.cbCliente.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Año";
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(16, 149);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(150, 20);
+            this.txtYear.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "N° Motor";
+            // 
+            // txtEnngine
+            // 
+            this.txtEnngine.Location = new System.Drawing.Point(16, 186);
+            this.txtEnngine.Name = "txtEnngine";
+            this.txtEnngine.Size = new System.Drawing.Size(150, 20);
+            this.txtEnngine.TabIndex = 22;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "N° Chassis";
+            // 
+            // txtChassis
+            // 
+            this.txtChassis.Location = new System.Drawing.Point(16, 223);
+            this.txtChassis.Name = "txtChassis";
+            this.txtChassis.Size = new System.Drawing.Size(150, 20);
+            this.txtChassis.TabIndex = 24;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ckE);
+            this.groupBox1.Controls.Add(this.ckD);
+            this.groupBox1.Controls.Add(this.ckG);
+            this.groupBox1.Location = new System.Drawing.Point(16, 246);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(147, 89);
+            this.groupBox1.TabIndex = 25;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Combustible";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // ckE
+            // 
+            this.ckE.AutoSize = true;
+            this.ckE.Location = new System.Drawing.Point(9, 66);
+            this.ckE.Name = "ckE";
+            this.ckE.Size = new System.Drawing.Size(67, 17);
+            this.ckE.TabIndex = 2;
+            this.ckE.Text = "Electrico";
+            this.ckE.UseVisualStyleBackColor = true;
+            // 
+            // ckD
+            // 
+            this.ckD.AutoSize = true;
+            this.ckD.Location = new System.Drawing.Point(9, 42);
+            this.ckD.Name = "ckD";
+            this.ckD.Size = new System.Drawing.Size(55, 17);
+            this.ckD.TabIndex = 1;
+            this.ckD.Text = "Diesel";
+            this.ckD.UseVisualStyleBackColor = true;
+            // 
+            // ckG
+            // 
+            this.ckG.AutoSize = true;
+            this.ckG.Location = new System.Drawing.Point(9, 19);
+            this.ckG.Name = "ckG";
+            this.ckG.Size = new System.Drawing.Size(67, 17);
+            this.ckG.TabIndex = 0;
+            this.ckG.Text = "Gasolina";
+            this.ckG.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 336);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 13);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Tipo Vehiculo";
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(16, 350);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(150, 20);
+            this.txtType.TabIndex = 27;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 372);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Capacidad";
+            // 
+            // txtCapacity
+            // 
+            this.txtCapacity.Location = new System.Drawing.Point(16, 385);
+            this.txtCapacity.Name = "txtCapacity";
+            this.txtCapacity.Size = new System.Drawing.Size(150, 20);
+            this.txtCapacity.TabIndex = 29;
+            // 
+            // Vehicles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 503);
+            this.Controls.Add(this.txtCapacity);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtChassis);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEnngine);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbCliente);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbModel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Name = "Vehicles";
+            this.Text = "Vehicles";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbModel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbCliente;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEnngine;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtChassis;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox ckE;
+        private System.Windows.Forms.CheckBox ckD;
+        private System.Windows.Forms.CheckBox ckG;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtCapacity;
+    }
+}
